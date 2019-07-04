a9191a3ecb7484b91f08b123765adf915e330e12
Vincenzo di Somma <vincidis@gmail.com>
2019-07-04 14:15:29 +0200

    Initial Commit

---
+++ myFirstARapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFirstARapp
+//
+//  Created by Vincenzo di Somma on 04/07/2019.
+//  Copyright © 2019 Vincenzo di Somma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
